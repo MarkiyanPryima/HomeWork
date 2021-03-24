92d71e33f20316f440271135079405f0ca08bea4
MarkiyanPryima <81187759+MarkiyanPryima@users.noreply.github.com>
2021-03-24 18:06:37 +0200

    Add files via upload
    HW1

---
+++ HW1/Task6/Task6.js
@@ -0,0 +1,12 @@
+const name = "Markiyan";
+const surname = "Pryima";
+const group = 12;
+const birhYear = null;
+const familyStatus = true;
+const someVariable = undefined;
+
+console.log(typeof group);
+console.log(typeof familyStatus);
+console.log(typeof name);
+console.log(typeof birhYear);
+console.log(typeof someVariable);
